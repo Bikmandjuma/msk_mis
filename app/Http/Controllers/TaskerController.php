3f710103bf375f4e1a5afbdc18c7d79d1c14e4b6
BikmanDjuma <bikmangeek@gmail.com>
2022-09-03 04:18:41 -0700

    tes

--- app/Http/Controllers/TaskerController.php
+++ app/Http/Controllers/TaskerController.php
@@ -159,10 +159,9 @@ class TaskerController extends Controller
     public function SolvingComplains($id){
         date_default_timezone_set('Africa/Kigali');
         $date=date('d-m-Y');
-        $time=date('H:i:s');
         $done='solved';
         $decision='done';
-        $profile=CitizenComplain::find($id)->update(['complains_reply'=>$done,'decision'=>$decision,'replied_date'=>$date,'replied_time'=>$time]);
+        $profile=CitizenComplain::find($id)->update(['complains_reply'=>$done,'decision'=>$decision,'replied_date'=>$date]);
         return redirect(url('tasker/solved/complains'));
     }
 
